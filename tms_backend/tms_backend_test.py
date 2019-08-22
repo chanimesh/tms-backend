2b6984e054bad9c2eb63ec4cfcd00afeedc4971c
Animesh <animeshc@thoughtworks.com>
2019-08-22 22:24:14 +0530

    trying monkey patching in flask with pytest

--- tms_backend/tms_backend_test.py
+++ tms_backend/tms_backend_test.py
@@ -1,6 +1,6 @@
 import pytest
 from tms_backend.tms_backend import tms_backend
-from flask import jsonify
+from .settings import mongo
 
 
 @pytest.fixture
@@ -16,3 +16,15 @@ def test_home_api(client):
             'description': 'This is a backend for a transaction management system'}
     assert response.is_json is True
     assert response.json == data
+
+
+def test_testdata_function(client, monkeypatch):
+    class MockDbObject:
+        class testCollection:
+            def find(self):
+                return {'value': 'hello'}
+
+    monkeypatch.setattr(mongo, 'db', MockDbObject)
+
+    response = client.get('/testdata')
+    assert b'the dummy data is  value' == response.data
